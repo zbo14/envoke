9d71ca544e0ced02793ad3fddea288a9713aca09
zachary balder <zbalder68@gmail.com>
2017-02-25 14:42:29 -0500

    dsp analysis

--- dsp/analysis.go
+++ dsp/analysis.go
@@ -1,17 +1,80 @@
 package dsp
 
 import (
+	"encoding/binary"
 	"math"
 	"math/cmplx"
+
+	. "github.com/zbo14/envoke/common"
 )
+
+var (
+	NEIGHBORHOOD_SIZE    = 20
+	OVERLAP_RATIO        = 0.5
+	SAMPLING_RATE        = 44100
+	SIMILARITY_THRESHOLD = 0.3
+	WINDOW               = HammingWindow
+	WINDOW_SIZE          = 4096
+)
+
+func Fingerprint(dists []float64) []byte {
+	p := make([]byte, 8*len(dists))
+	for i, dist := range dists {
+		binary.BigEndian.PutUint64(p[i*8:(i+1)*8], math.Float64bits(dist))
+	}
+	return Checksum256(p)
+}
+
+func SimilarityMeasure(dists1, dists2 []float64) (diff float64) {
+	var i int
+	l1 := len(dists1)
+	l2 := len(dists2)
+	if l1 == l2 {
+		for i = range dists1 {
+			diff += math.Pow(dists1[i]-dists2[i], 2)
+		}
+		return diff / float64(l1)
+	} else if l1 < l2 {
+		for i = 0; i+l1 < l2; i++ {
+			diff += SimilarityMeasure(dists1, dists2[i:i+l1])
+		}
+	} else {
+		for i = 0; i+l2 < l1; i++ {
+			diff += SimilarityMeasure(dists1[i:i+l2], dists2)
+		}
+	}
+	return diff
+}
+
+func PeakDistances(freqs []float64, peaks [][]bool) (dists []float64) {
+	freqMin, freqMax := freqs[0], freqs[len(freqs)-1]
+	freqRange := freqMax - freqMin
+	prevFreq := freqMin
+	prevTime := 0
+	n := len(peaks)
+	for i := range peaks {
+		for j := range peaks[i] {
+			if peaks[i][j] {
+				dists = append(dists, math.Pow((freqs[j]-prevFreq)/freqRange, 2)+math.Pow(float64(i-prevTime)/float64(n-1), 2))
+				prevFreq, prevTime = freqs[j], i
+			}
+		}
+	}
+	return
+}
+
+func DefaultFindPeaks(sgram [][]float64) [][]bool {
+	return FindPeaks(NEIGHBORHOOD_SIZE, sgram)
+}
 
 func FindPeaks(nbr int, sgram [][]float64) [][]bool {
 	n := len(sgram)
 	peaks := make([][]bool, n)
-	for i, x := range s {
+	for i, x := range sgram {
 	OUTER:
 		for j := range x {
 			m := len(x)
+			peaks[i] = make([]bool, m)
 			for k := 0; k <= nbr; k++ {
 				if k != 0 {
 					if j-k >= 0 {
@@ -27,12 +90,12 @@ func FindPeaks(nbr int, sgram [][]float64) [][]bool {
 				}
 				for b := 1; b <= nbr-k; b++ {
 					if i-b >= 0 {
-						if x[j] < s[i-b][k] {
+						if x[j] < sgram[i-b][k] {
 							continue OUTER
 						}
 					}
 					if i+b < n {
-						if x[j] < s[i+b][k] {
+						if x[j] < sgram[i+b][k] {
 							continue OUTER
 						}
 					}
@@ -44,23 +107,38 @@ func FindPeaks(nbr int, sgram [][]float64) [][]bool {
 	return peaks
 }
 
-func FftSpectrogram(fs float64, l int, olap float64, win func(int) []float64, x []float64) ([]float64, [][]float64, error) {
+func DefaultFftSpectrogram(x []float64) ([]float64, [][]float64, error) {
+	return FftSpectrogram(SAMPLING_RATE, WINDOW_SIZE, OVERLAP_RATIO, WINDOW, x)
+}
+
+func FftSpectrogram(fs, l int, olap float64, win func(int) []float64, x []float64) ([]float64, [][]float64, error) {
 	l2p1 := l>>1 + 1
 	sgram, err := LengthOverlap(l, olap, x)
 	if err != nil {
 		return nil, nil, err
 	}
 	for i := range sgram {
-		ApplyWindow(win, s[i])
-		z := FftReal(s[i])
+		ApplyWindow(win, sgram[i])
+		z := FftReal(sgram[i])
+		min, max := math.Inf(1), math.Inf(-1)
 		for j := 0; j < l2p1; j++ {
-			sgram[i][j] = math.Log10(math.Sqrt(real(z[j] * cmplx.Conj(z[j]))))
+			sgram[i][j] = math.Sqrt(real(z[j] * cmplx.Conj(z[j])))
+			if sgram[i][j] < min {
+				min = sgram[i][j]
+			}
+			if sgram[i][j] > max {
+				max = sgram[i][j]
+			}
+		}
+		for j := 0; j < l2p1; j++ {
+			sgram[i][j] -= min
+			sgram[i][j] /= (max - min)
 		}
 		sgram[i] = sgram[i][:l2p1]
 	}
 	freqs := make([]float64, l2p1)
 	for i := range freqs {
-		freqs[i] = float64(i) * fs / float64(l)
+		freqs[i] = float64(i) * float64(fs) / float64(l)
 	}
 	return freqs, sgram, nil
 }
--- dsp/dsp_test.go
+++ dsp/dsp_test.go
@@ -5,7 +5,10 @@ import (
 	"testing"
 )
 
-var path = "/Users/zach/Desktop/music/Allegro from Duet in C Major.mp3"
+var (
+	path1 = "/Users/zach/Desktop/music/rhapsody_1.mp3"
+	path2 = "/Users/zach/Desktop/music/rhapsody_2.mp3"
+)
 
 func TestDsp(t *testing.T) {
 	// FFT
@@ -14,17 +17,22 @@ func TestDsp(t *testing.T) {
 	// InvDecimationFreq(z)
 	// FftDecimationTime(a)
 	// t.Log(InvReal(z))
-	file := MustOpenFile(path)
+	file := MustOpenFile(path1)
 	x := MustReadTimeDomain(file)
-	s, freqs, err := FftSpectrogram(44100, 1024, 0.5, BlackmanWindow, x)
+	freqs, sgram, err := DefaultFftSpectrogram(x)
 	if err != nil {
 		t.Fatal(err)
 	}
-	file = MustOpenWriteFile("test.json")
-	t.Log(s)
-	t.Log(freqs)
-	// Println(out)
-	// Convolution
-	// out := FftConvolution(0.21, 398, 625, x)
-	// t.Log(out)
+	peaks := DefaultFindPeaks(sgram)
+	dists1 := PeakDistances(freqs, peaks)
+	file = MustOpenFile(path2)
+	x = MustReadTimeDomain(file)
+	freqs, sgram, err = DefaultFftSpectrogram(x)
+	if err != nil {
+		t.Fatal(err)
+	}
+	peaks = DefaultFindPeaks(sgram)
+	dists2 := PeakDistances(freqs, peaks)
+	sim := SimilarityMeasure(dists1, dists2)
+	t.Log(sim)
 }

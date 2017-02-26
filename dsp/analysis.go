44310276a8d3710246497c9ae63f0cf2813f4f52
zachary balder <zbalder68@gmail.com>
2017-02-26 14:39:57 -0500

    lccs similarity

--- dsp/analysis.go
+++ dsp/analysis.go
@@ -1,6 +1,7 @@
 package dsp
 
 import (
+	"bytes"
 	"encoding/binary"
 	"math"
 	"math/cmplx"
@@ -9,52 +10,75 @@ import (
 )
 
 var (
-	FAN_VALUE            = 15
-	NEIGHBORHOOD_SIZE    = 20
-	OVERLAP_RATIO        = 0.5
-	SAMPLING_RATE        = 44100
-	SIMILARITY_THRESHOLD = 0.1
-	WINDOW               = HammingWindow
-	WINDOW_SIZE          = 4096
+	FAN_VALUE         = 15
+	TIME_DELTA        = 200
+	NEIGHBORHOOD_SIZE = 20
+	OVERLAP_RATIO     = 0.5
+	SAMPLING_RATE     = 44100
+	WINDOW            = HammingWindow
+	WINDOW_SIZE       = 4096
 )
 
-func DefaultCompareConstellations(c1, c2 map[string]struct{}) bool {
-	return CompareConstellations(c1, c2, SIMILARITY_THRESHOLD)
-}
-
-func CompareConstellations(c1, c2 map[string]struct{}, threshold float64) bool {
-	shared := 0
-	for fprint := range c1 {
-		if _, ok := c2[fprint]; ok {
-			shared++
+func CompareConstellations(c1, c2 [][]byte) float64 {
+	n, m := len(c1), len(c2)
+	if n == 0 || m == 0 {
+		return 0
+	}
+	lseq := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if bytes.Equal(c1[i], c2[j]) {
+				a, b := i, j
+				prev, seq := b, 1
+				for {
+					if a++; a == n {
+						break
+					}
+					for {
+						if b++; b == m {
+							b = prev
+							break
+						}
+						if bytes.Equal(c1[a], c2[b]) {
+							prev = b
+							seq++
+							break
+						}
+					}
+				}
+				if seq > lseq {
+					lseq = seq
+				}
+			}
 		}
 	}
-	ratio := 2 * float64(shared) / float64(len(c1)+len(c2))
-	Println(ratio)
-	return ratio >= threshold
+	if n < m {
+		return float64(lseq) / float64(n)
+	}
+	return float64(lseq) / float64(m)
 }
 
-func DefaultConstellation(peakFreqs [][]float64) map[string]struct{} {
-	return Constellation(FAN_VALUE, peakFreqs)
+func DefaultConstellation(peaks [][]float64) [][]byte {
+	return Constellation(FAN_VALUE, peaks, TIME_DELTA)
 }
 
-func Constellation(fan int, peakFreqs [][]float64) map[string]struct{} {
-	fprints := make(map[string]struct{})
-	n := len(peakFreqs)
-	for i, freqs := range peakFreqs {
-		m := len(freqs)
+func Constellation(fan int, peaks [][]float64, tdelta int) [][]byte {
+	var fprints [][]byte
+	n := len(peaks)
+	for i := range peaks {
+		m := len(peaks[i])
 	OUTER:
-		for j := range freqs {
+		for j := 0; j < m; j++ {
 			rem := fan
 			for k := j + 1; k < m; k++ {
-				fprints[Fingerprint(freqs[j], freqs[k], 0)] = struct{}{}
+				fprints = append(fprints, Fingerprint(peaks[i][j], peaks[i][k], 0))
 				if rem--; rem == 0 {
 					continue OUTER
 				}
 			}
-			for a := i + 1; a < n; a++ {
-				for b := 0; b < len(peakFreqs[a]); b++ {
-					fprints[Fingerprint(freqs[j], peakFreqs[a][b], a-i)] = struct{}{}
+			for a := i + 1; a < n && (tdelta <= 0 || a-i <= tdelta); a++ {
+				for b := 0; b < len(peaks[a]); b++ {
+					fprints = append(fprints, Fingerprint(peaks[i][j], peaks[a][b], a-i))
 					if rem--; rem == 0 {
 						continue OUTER
 					}
@@ -65,55 +89,147 @@ func Constellation(fan int, peakFreqs [][]float64) map[string]struct{} {
 	return fprints
 }
 
-func Fingerprint(freq1, freq2 float64, tdelta int) string {
+func Fingerprint(peak1, peak2 float64, tdelta int) []byte {
 	p := make([]byte, 16)
-	binary.BigEndian.PutUint64(p[0:8], math.Float64bits(freq1))
-	binary.BigEndian.PutUint64(p[8:], math.Float64bits(freq2))
+	binary.BigEndian.PutUint64(p[0:8], math.Float64bits(peak1))
+	binary.BigEndian.PutUint64(p[8:], math.Float64bits(peak2))
 	p = append(p, Uint64Bytes(tdelta)...)
-	return BytesToHex(Checksum256(p))
+	return Checksum256(p)
 }
 
-func DefaultPeakFrequencies(freqs []float64, sgram [][]float64) [][]float64 {
-	return PeakFrequencies(freqs, NEIGHBORHOOD_SIZE, sgram)
+func CompareDistances(dists1, dists2 []float64, eps float64) float64 {
+	n, m := len(dists1), len(dists2)
+	if n == 0 || m == 0 {
+		return 0
+	}
+	lseq := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if math.Abs(dists1[i]-dists2[j]) < eps {
+				a, b := i, j
+				prev, seq := b, 1
+				for {
+					if a++; a == n {
+						break
+					}
+					for {
+						if b++; b == m {
+							b = prev
+							break
+						}
+						if math.Abs(dists1[a]-dists2[b]) < eps {
+							prev = b
+							seq++
+							break
+						}
+					}
+				}
+				if seq > lseq {
+					lseq = seq
+				}
+			}
+		}
+	}
+	if n < m {
+		return float64(lseq) / float64(n)
+	}
+	return float64(lseq) / float64(m)
 }
 
-func PeakFrequencies(freqs []float64, nbr int, sgram [][]float64) [][]float64 {
+func DefaultFindDistances(peaks [][]float64) []float64 {
+	return FindDistances(FAN_VALUE, peaks, TIME_DELTA)
+}
+
+func FindDistances(fan int, peaks [][]float64, tdelta int) []float64 {
+	dists := make([]float64, 1024)
+	idx, n := 0, len(peaks)
+	for i := range peaks {
+		m := len(peaks[i])
+	OUTER:
+		for j := 0; j < m; j++ {
+			rem := fan
+			for k := j + 1; k < m; k++ {
+				if idx == len(dists) {
+					dists = append(dists, make([]float64, 1024)...)
+				}
+				dists[idx] = math.Abs(peaks[i][j] - peaks[i][k])
+				idx++
+				if rem--; rem == 0 {
+					continue OUTER
+				}
+			}
+			for a := i + 1; a < n && (tdelta <= 0 || a-i <= tdelta); a++ {
+				for b := 0; b < len(peaks[a]); b++ {
+					if idx == len(dists) {
+						dists = append(dists, make([]float64, 1024)...)
+					}
+					dists[idx] = math.Abs(peaks[i][j] - peaks[a][b])
+					idx++
+					if rem--; rem == 0 {
+						continue OUTER
+					}
+				}
+			}
+		}
+	}
+	return dists[:idx]
+}
+
+func DefaultFindPeaks(freqs []float64, sgram [][]float64) [][]float64 {
+	return FindPeaks(freqs, NEIGHBORHOOD_SIZE, sgram)
+}
+
+func FindPeaks(freqs []float64, nbr int, sgram [][]float64) [][]float64 {
 	n := len(sgram)
-	peakFreqs := make([][]float64, n)
+	peaks := make([][]float64, n)
 	for i, x := range sgram {
+		m := len(x)
 	OUTER:
 		for j := range x {
-			m := len(x)
 			for k := 0; k <= nbr; k++ {
-				if k != 0 {
+				if k > 0 {
 					if j-k >= 0 {
-						if x[j] < x[j-k] {
+						if x[j] <= x[j-k] {
 							continue OUTER
 						}
 					}
 					if j+k < m {
-						if x[j] < x[j+k] {
+						if x[j] <= x[j+k] {
 							continue OUTER
 						}
 					}
 				}
 				for b := 1; b <= nbr-k; b++ {
 					if i-b >= 0 {
-						if x[j] < sgram[i-b][k] {
-							continue OUTER
+						if j-k >= 0 {
+							if x[j] <= sgram[i-b][j-k] {
+								continue OUTER
+							}
+						}
+						if j+k < m {
+							if x[j] <= sgram[i-b][j+k] {
+								continue OUTER
+							}
 						}
 					}
 					if i+b < n {
-						if x[j] < sgram[i+b][k] {
-							continue OUTER
+						if j-k >= 0 {
+							if x[j] <= sgram[i+b][j-k] {
+								continue OUTER
+							}
+						}
+						if j+k < m {
+							if x[j] <= sgram[i+b][j+k] {
+								continue OUTER
+							}
 						}
 					}
 				}
 			}
-			peakFreqs[i] = append(peakFreqs[i], freqs[j])
+			peaks[i] = append(peaks[i], x[j])
 		}
 	}
-	return peakFreqs
+	return peaks
 }
 
 func DefaultFftSpectrogram(x []float64) ([]float64, [][]float64, error) {
@@ -130,7 +246,7 @@ func FftSpectrogram(fs, l int, olap float64, win func(int) []float64, x []float6
 		ApplyWindow(win, sgram[i])
 		z := FftReal(sgram[i])
 		for j := 0; j < l2p1; j++ {
-			sgram[i][j] = math.Log10(math.Sqrt(real(z[j] * cmplx.Conj(z[j]))))
+			sgram[i][j] = math.Log10(real(z[j] * cmplx.Conj(z[j])))
 		}
 		sgram[i] = sgram[i][:l2p1]
 	}
@@ -140,33 +256,3 @@ func FftSpectrogram(fs, l int, olap float64, win func(int) []float64, x []float6
 	}
 	return freqs, sgram, nil
 }
-
-/*
-func SimilarityMeasure(dists1, dists2 []float64) (diff float64) {
-	l1 := len(dists1)
-	l2 := len(dists2)
-	if l1 == l2 {
-		for i, dist := range dists1 {
-			diff += math.Pow(dist-dists2[i], 2)
-		}
-		return diff
-	} else if l1 < l2 {
-		diff = math.Inf(1)
-		for i := 0; i+l1 <= l2; i++ {
-			d := SimilarityMeasure(dists1, dists2[i:i+l1])
-			if d < diff {
-				diff = d
-			}
-		}
-	} else {
-		diff = math.Inf(1)
-		for i := 0; i+l2 < l1; i++ {
-			d := SimilarityMeasure(dists1[i:i+l2], dists2)
-			if d < diff {
-				diff = d
-			}
-		}
-	}
-	return diff
-}
-*/
--- dsp/dsp_test.go
+++ dsp/dsp_test.go
@@ -11,27 +11,42 @@ var (
 )
 
 func TestDsp(t *testing.T) {
-	// FFT
-	// x := []float64{1.0, 2.0, 3.2, 4.5, 6.7, 7.01, 8.9, 10}
-	// z := FftReal(x)
-	// InvDecimationFreq(z)
-	// FftDecimationTime(a)
-	// t.Log(InvReal(z))
+	/*
+		// FFT
+		x := []float64{1.0, 2.0, 3.2, 4.5, 6.7, 7.01, 8.9, 10}
+		z := FftReal(x)
+		InvDecimationFreq(z)
+		FftDecimationTime(a)
+		t.Log(InvReal(z))
+
+		// PEAKS
+		freqs := []float64{100, 200, 300, 400, 500, 600, 700, 800}
+		sgram := [][]float64{
+			[]float64{1, 4, 2, 4, 10, 1, 2, 4},
+			[]float64{1, 2, 3, 4, 5, 6, 7, 8},
+			[]float64{3, 5, 6, 2, 3, 4, 3, 3},
+			[]float64{3, 1, 6, 2, 2, 12, 3, 3},
+		}
+		t.Log(FindPeaks(freqs, 3, sgram))
+	*/
+
 	file := MustOpenFile(path1)
 	x := MustReadTimeDomain(file)
 	freqs, sgram, err := DefaultFftSpectrogram(x)
 	if err != nil {
 		t.Fatal(err)
 	}
-	peakFreqs := DefaultPeakFrequencies(freqs, sgram)
-	constellation1 := DefaultConstellation(peakFreqs)
+	peaks := DefaultFindPeaks(freqs, sgram)
+	dists1 := DefaultFindDistances(peaks)
+
 	file = MustOpenFile(path2)
 	x = MustReadTimeDomain(file)
 	freqs, sgram, err = DefaultFftSpectrogram(x)
 	if err != nil {
 		t.Fatal(err)
 	}
-	peakFreqs = DefaultPeakFrequencies(freqs, sgram)
-	constellation2 := DefaultConstellation(peakFreqs)
-	t.Log(DefaultCompareConstellations(constellation1, constellation2))
+	peaks = DefaultFindPeaks(freqs, sgram)
+	dists2 := DefaultFindDistances(peaks)
+
+	t.Log(CompareDistances(dists1, dists2, 0.04*float64(SAMPLING_RATE)))
 }
